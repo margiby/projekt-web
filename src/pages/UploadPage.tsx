80272d988799abe1c717d6f607b4b1dedff7d8f8
Margarita <131612561+margiby@users.noreply.github.com>
2025-05-08 10:53:00 +0200

    Update UploadPage.tsx

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
 
-const UploadPage = () => 
-<div className="container">
-    Upload-Seite
-    </div>;
+const UploadPage: React.FC = () => {
+    return (
+        <div className="container">Upload-Seite</div>
+    );
+};
 export default UploadPage;
